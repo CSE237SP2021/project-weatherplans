17d1f91e811aa63a23456f3630c8887264135d0e
Anh <anhsle15@gmail.com>
2021-03-24 15:39:46 -0500

    adding basic argument functionality

--- src/weatherapp/menu.java
+++ src/weatherapp/menu.java
@@ -4,7 +4,14 @@ public class menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		if(args.length == 1) {
+			if(args[0].equals("--help")) {
+				System.out.println("get help lmao");
+			}
+		}
+		else {
+			System.out.println("we doing other stuff");
+		}
 	}
 
 }
