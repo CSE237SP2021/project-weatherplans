e75af3b4adab001b6bbd5e31cb08c1a276c2b314
Evan Han <evanhan@wustl.edu>
2021-03-26 09:54:16 -0500

    Added junit tests to build path.

---
+++ src/test/java/weatherplans/OpenWeatherApiTests.java
@@ -0,0 +1,14 @@
+package weatherplans;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OpenWeatherApiTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
