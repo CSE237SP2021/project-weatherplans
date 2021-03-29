19f1a07da3fa45a7dd0b260716742f851fa8f0b3
Anh <anhsle15@gmail.com>
2021-03-29 11:00:23 -0500

    created parse testing file

---
+++ src/test/java/weatherplans/ArgumentParserTest.java
@@ -0,0 +1,14 @@
+package weatherplans;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArgumentParserTest {
+
+	@Test
+	void testParseId() {
+		
+	}
+
+}
